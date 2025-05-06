ba8fcbd0d99d57c301d89ba0f04afc9a34ff575a
jharmez007 <jamesagu2001@gmail.com>
2025-05-06 14:40:56 +0100

    status code update

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -93,7 +93,7 @@ const Login = () => {
       const response = await login({ email, password });
     
       const token = response?.data?.access_token;
-      if (response.status === 201) {
+      if (response.status === 200) {
         localStorage.setItem("token", token);
         showToast("Login successful!", "success");
         setTimeout(() => navigate("/shipper-dashboard"), 1500);
